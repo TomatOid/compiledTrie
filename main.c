45d90e7091196fd37a8ad62419604b6630657f77
Connor <connormc757@gmail.com>
2020-06-14 22:05:59 -0400

    First working version

--- main.c
+++ main.c
@@ -2,17 +2,51 @@
 #include <stdlib.h>
 #include <string.h>
 
+// format description: "key" { action }
+
 typedef struct KeyAction
 {
     char *key;
     char *action;
-};
+} KeyAction;
+
+int compareKeyAction(const void *a, const void *b)
+{
+    return -strcmp(((KeyAction *)a)->key, ((KeyAction *)b)->key);
+}
+
+void recursivelyCompileTrie(FILE *out_file, KeyAction *array, size_t length, size_t word_offset)
+{
+    fprintf(out_file, "switch *c {\n");
+    for (size_t i = 0; i < length; )
+    {
+        size_t j;
+        for (j = i; j < length && (array[i].key[word_offset] == array[j].key[word_offset]); j++);
+        if (array[i].key[word_offset])
+        {
+            fprintf(out_file, "case \'%c\':\n", array[i].key[word_offset]);
+            fprintf(out_file, "c++;\n");
+            recursivelyCompileTrie(out_file, &array[i], j - i, word_offset + 1);
+            i = j;
+            fprintf(out_file, "break;\n");
+        }
+        else
+        {
+            fprintf(out_file, "case \'\\0\':\n");
+            fprintf(out_file, "%s", array[i].action);
+            fprintf(out_file, "break;\n");
+            i++;
+        }
+    }
+    fprintf(out_file, "default: \nreturn 0;\n}\n");
+    return;
+}
 
 int main(int argc, char **argv)
 {
     if (argc < 3)
     {
-        printf("usage: %s infile outfile", argv[0]);
+        printf("usage: %s infile outfile\n", argv[0]);
         exit(EXIT_FAILURE);
     }
 
@@ -35,51 +69,120 @@ int main(int argc, char **argv)
     fread(file_buffer, 1, input_file_size, input_file);
     fclose(input_file);
     char *current_char = file_buffer;
-
+    file_buffer[input_file_size - 1] = EOF;
     do
     {
-        if (is_escaped)
+        // brackets have the highest precedence
+        if (brackets_depth > 0)
         {
-            buffer_size++;
-            is_escaped = 0;
-        }
-        else
-        {
-            // if we are in curly braces
-            if (brackets_depth > 0)
+            switch (*current_char)
             {
-                switch *current_char
+                case '{':
+                    brackets_depth++;
+                    break;
+                case '}':
+                    brackets_depth--;
+                    if (!brackets_depth)
+                    {
+                        *current_char = '\0';
+                    }
+                    break;
+                default:
+                    break;
+            }
+            buffer_size++;
+        }
+        else if (is_quoted)
+        {
+            if (is_escaped)
+            {
+                buffer_size++;
+                is_escaped = 0;
+            }
+            else
+            {
+                switch (*current_char)
                 {
-                    case '{':
-                        brackets_depth++;
-                        buffer_size++;
+                    case '\\':
+                        is_escaped = 1;
                         break;
-                    case '}':
-                        // say we have input ... { if (a) { doThing(); } } ... we don't want to count the two outer braces towards buffer_size
-                        // this part deals with the last brace (when brackets_depth goes from one to zero)
-                        if (brackets_depth--)
-                        {
-                            buffer_size++;
-                        }
+                    case '\"':
+                        *current_char = '\0';
+                        buffer_size++;
+                        number_of_keys++;
+                        is_quoted = 0;
                         break;
                     default:
                         buffer_size++;
+                        break;
                 }
             }
-            else if (*current_char == '{')
+        }
+        else
+        {
+            switch (*current_char)
             {
-                brackets_depth++;
-            }
-            else if (*current_char == '\"')
-            {
-                if (!is_quoted) { number_of_keys++; }
-                is_quoted = !is_quoted;
-            }
-            else if (is_quoted)
-            {
-                buffer_size++;
+                case '{':
+                    brackets_depth++;
+                    break;
+                case '\"':
+                    is_quoted = 1;
+                    break;
+                default:
+                    break;
             }
         }
-    } while (current_char++ != EOF);
+    } while (*(current_char++) != EOF);
+    printf("%d\n", number_of_keys);
+
+    KeyAction *pairs = calloc(number_of_keys, sizeof(KeyAction));
     
-    string_buffer
+    // rewind the current_char
+    current_char = file_buffer;
+
+    enum { TYPE_KEY, TYPE_ACTION };
+    int last_type = TYPE_ACTION;
+    size_t key_index = 0;
+
+    do
+    {
+        switch (*current_char)
+        {
+            case '\"':
+                current_char++;
+                if (last_type == TYPE_KEY)
+                {
+                    printf("Two keys in a row %s\n", current_char);
+                    exit(EXIT_FAILURE);
+                }
+                last_type = TYPE_KEY;
+                pairs[key_index].key = current_char;
+                current_char += strlen(current_char) + 1;
+                break;
+            case '{':
+                current_char++;
+                if (last_type == TYPE_ACTION)
+                {
+                    printf("An action must be preceded by a key %s\n", current_char);
+                    exit(EXIT_FAILURE);
+                }
+                last_type = TYPE_ACTION;
+                pairs[key_index].action = current_char;
+                key_index++;
+                current_char += strlen(current_char) + 1;
+                break;
+            default:
+                current_char++;
+                break;
+        }
+    } while (*current_char != EOF);
+    
+    qsort((void*)pairs, number_of_keys, sizeof(KeyAction), compareKeyAction);
+    for (int i = 0; i < number_of_keys; i++)
+    {
+        printf("%s, %s\n", pairs[i].key, pairs[i].action);
+    }
+    recursivelyCompileTrie(output_file, pairs, number_of_keys, 0);
+}
+
+
